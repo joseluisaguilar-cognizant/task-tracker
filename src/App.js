07532dbb89c7e7109c6f19f0d3abd05e49d9eeb6
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-14 15:47:08 +0100

    Get tasks from an axios call to a local json-server

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 import Header from "./components/Header/Header";
 import Tasks from "./components/Tasks/Tasks";
 import AddTask from "./components/AddTask/AddTask";
-import { taskList } from "./components/Tasks/TasksData";
 
 function App() {
-  const [tasks, setTasks] = useState(taskList);
+  const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/tasks").then(({ data }) => {
+      setTasks(data);
+    });
+  }, []);
 
   const deleteTask = (id) => {
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
