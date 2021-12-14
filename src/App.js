ac6078c35931ba1cb1444192635a66a51d4f2084
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-14 11:56:48 +0100

    Create global State for APP component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useState } from "react";
+
 import Header from "./components/Header/Header";
+import Tasks from "./components/Tasks/Tasks";
+import { taskList } from "./components/Tasks/TasksData";
 
 function App() {
+  const [tasks, setTasks] = useState(taskList);
+
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks} />
     </div>
   );
 }
