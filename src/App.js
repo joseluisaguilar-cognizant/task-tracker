d931a7335dce439aafc015582610900d3d028804
joseluisaguilar-cognizant <JoseLuis.AguilarGomez@cognizant.com>
2021-12-14 17:06:17 +0100

    Interact with REST API

--- src/App.js
+++ src/App.js
@@ -5,32 +5,72 @@ import Header from "./components/Header/Header";
 import Tasks from "./components/Tasks/Tasks";
 import AddTask from "./components/AddTask/AddTask";
 
+const TASK_URL = "http://localhost:5000/tasks";
+
+const headers = {
+  "Content-type": "application/json",
+};
+
 function App() {
   const [tasks, setTasks] = useState([]);
   const [showAddTask, setShowAddTask] = useState(false);
 
   useEffect(() => {
-    axios.get("http://localhost:5000/tasks").then(({ data }) => {
-      setTasks(data);
-    });
+    // axios.get(TASK_URL, headers).then((res) => {
+    //   console.log(res.json());
+    //   setTasks(res.data);
+    // });
+
+    // -> It would be better using async-await
+    const storeTasks = async () => {
+      const tasks = await fetchTasks();
+
+      setTasks(tasks);
+    };
+
+    storeTasks();
   }, []);
 
+  const fetchTasks = async () => {
+    const res = await axios.get(TASK_URL, {
+      headers,
+    });
+    return res.data;
+  };
+
+  const fetchSpecificTask = async (id) => {
+    const res = await axios.get(`${TASK_URL}/${id}`, { headers });
+    return res.data;
+  };
+
+  const addTask = async (task) => {
+    const { data: newTask } = await axios.post(TASK_URL, task, { headers });
+
+    setTasks((prevTasks) => {
+      return [...prevTasks, newTask];
+    });
+  };
+
   const deleteTask = (id) => {
+    axios.delete(`${TASK_URL}/${id}`, { headers });
+
     setTasks((prevTasks) => prevTasks.filter((task) => task.id !== id));
   };
 
-  const toggleReminder = (id) => {
-    setTasks((prevTaks) => {
-      return prevTaks.map((task) =>
-        task.id === id ? { ...task, reminder: !task.reminder } : task
-      );
-    });
-  };
+  const toggleReminder = async (id) => {
+    const taskToToggle = await fetchSpecificTask(id);
 
-  const addTask = (task) => {
-    const id = Math.floor(Math.random() * 1000);
-    setTasks((prevTasks) => {
-      return [...prevTasks, { ...task, id }];
+    console.log(taskToToggle);
+
+    const res = await axios.put(`${TASK_URL}/${id}`, {
+      ...taskToToggle,
+      reminder: !taskToToggle.reminder,
+    });
+
+    const taskUpdated = res.data;
+
+    setTasks((prevTaks) => {
+      return prevTaks.map((task) => (task.id === id ? taskUpdated : task));
     });
   };
 
